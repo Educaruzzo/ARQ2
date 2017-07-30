4322aed1c73a34e837f5dfbd1f103cb21b87d1ff
Educaruzzo <30328949+Educaruzzo@users.noreply.github.com>
2017-07-30 18:44:56 -0300

    Update MemoriaCache.h

--- MemoriaCache.h
+++ MemoriaCache.h
@@ -88,6 +88,7 @@ void readDataCache(unsigned int address) {		// Utiliza o CDB
 		writeDataCache_ViaCMB();
 		
 		buffer = CMB.dados;
+		
 		InsereFilaCDB(&FilaCDB, buffer);
 		//INCREMENTA CICLOS GASTOS
 	}
@@ -105,7 +106,7 @@ void writeDataCache_ViaCMB() {
 		buffer = CMB.dados;
 		buffer02 = CMB.address;
 		
-		CMB.address = ((CacheDados.tag[line] << (32 - NUM_BITS_TAG))|(line << NUM_BITS_TAG)|(byte));
+		CMB.address = ((CacheDados.tag[line] << (32 - NUM_BITS_TAG))|(line << NUM_BITS_BYTE)|(0));
 		//Faz o Write-Back
 		switch (byte) {
 			case 0:
@@ -151,95 +152,29 @@ void writeDataCache_ViaCMB() {
 	// INCREMENTA CICLOS GASTOS
 }
 
-
-void readInstructionCache(unsigned int address) {		// Utiliza o CDB
-	int line, tag, byte;
-	int buffer;					// Simboliza um buffer genérico
-	
-	line = (address << NUM_BITS_TAG) >> (32 - NUM_BITS_LINHA);
-	
-	if (CacheDados.estado[line] == VALIDO) {
-		tag = address >> (32 - NUM_BITS_TAG);
-		
-		if (CacheDados.tag[line] == tag) {
-			byte = (address << (32 - NUM_BITS_BYTE)) >> (32 - NUM_BITS_BYTE);
-			
-			switch (byte) {
-				case 0:
-					InsereFilaCDB(&FilaCDB, CacheDados.Linha[line].p1);
-					break;
-				case 1:
-					InsereFilaCDB(&FilaCDB, CacheDados.Linha[line].p2);
-					break;
-				case 2:
-					InsereFilaCDB(&FilaCDB, CacheDados.Linha[line].p3);
-					break;
-				case 3:
-					InsereFilaCDB(&FilaCDB, CacheDados.Linha[line].p4);
-					break;
-			}
-		}
-		else {
-			buffer = address;
-			CMB.address = (buffer >> NUM_BITS_BYTE) << NUM_BITS_BYTE;
-			CMB.controle = OCUPADO;
-			
-			readRAM();
-			writeDataCache_ViaCMB();
-			
-			buffer = CMB.dados;
-			InsereFilaCDB(&FilaCDB, buffer);
-			//INCREMENTA CICLOS GASTOS
-		}
-	}
-	else {
-		buffer = address;
-		CMB.address = (buffer >> NUM_BITS_BYTE) << NUM_BITS_BYTE;
-		CMB.controle = OCUPADO;
-		
-		readRAM();
-		writeDataCache_ViaCMB();
-		
-		buffer = CMB.dados;
-		InsereFilaCDB(&FilaCDB, buffer);
-		//INCREMENTA CICLOS GASTOS
-	}
-}
-
-void writeInstructionCache_ViaCMB() {
-	unsigned int line, tag, byte;
+void writeDataCache_ViaCDB() {
+	unsigned int line, tag, byte, i;
 	unsigned int buffer, buffer02;					// Simbolizam buffers genéricos
 	
-	line = (CMB.address << NUM_BITS_TAG) >> (32 - NUM_BITS_LINHA);
-	tag = CMB.address >> (32 - NUM_BITS_TAG);
-	byte = (CMB.address << (32 - NUM_BITS_BYTE)) >> (32 - NUM_BITS_BYTE);
+	line = (CDB.address << NUM_BITS_TAG) >> (32 - NUM_BITS_LINHA);
+	tag = CDB.address >> (32 - NUM_BITS_TAG);
+	byte = (CDB.address << (32 - NUM_BITS_BYTE)) >> (32 - NUM_BITS_BYTE);
 	
 	if (CacheDados.estado[line] == VALIDO) {
-		buffer = CMB.dados;
-		buffer02 = CMB.address;
-		
-		CMB.address = ((CacheDados.tag[line] << (32 - NUM_BITS_TAG))|(line << NUM_BITS_TAG)|(byte));
-		//Faz o Write-Back
-		switch (byte) {
-			case 0:
-				CMB.dados = CacheDados.Linha[line].p1;
-				writeRAM();
-				break;
-			case 1:
-				CMB.dados = CacheDados.Linha[line].p2;
-				writeRAM();
-				break;
-			case 2:
-				CMB.dados = CacheDados.Linha[line].p3;
-				writeRAM();
-				break;
-			case 3:
-				CMB.dados = CacheDados.Linha[line].p4;
-				writeRAM();
-				break;
+		if (CacheDados.tag[line] != tag) {
+			CMB.controle = OCUPADO;
+			CMB.address = ((CacheDados.tag[line] << (32 - NUM_BITS_TAG))|(line << NUM_BITS_BYTE)|(0));
+			
+			//Faz o Write-Back
+			CMB.dados = CacheDados.Linha[line].p1;
+			writeRAM();
+			CMB.dados = CacheDados.Linha[line].p2;
+			writeRAM();
+			CMB.dados = CacheDados.Linha[line].p3;
+			writeRAM();
+			CMB.dados = CacheDados.Linha[line].p4;
+			writeRAM();
 		}
-		CMB.dados = buffer;
-		CMB.address = buffer02;
 	}
 	
 	// Escreve dados na cache
@@ -258,6 +193,120 @@ void writeInstructionCache_ViaCMB() {
 				break;
 			case 3:
 				CacheDados.Linha[line].p4 = CMB.dados;
+				break;
+	}
+	
+	// INCREMENTA CICLOS GASTOS
+}
+
+
+void readInstructionCache() {		// Utiliza o CFB
+	int line, tag, byte;
+	int buffer;					// Simboliza um buffer genérico
+	
+	line = (CFB.address << NUM_BITS_TAG) >> (32 - NUM_BITS_LINHA);
+	
+	if (CacheInstrucoes.estado[line] == VALIDO) {
+		tag = CFB.address >> (32 - NUM_BITS_TAG);
+		
+		if (CacheInstrucoes.tag[line] == tag) {
+			byte = (CFB.address << (32 - NUM_BITS_BYTE)) >> (32 - NUM_BITS_BYTE);
+			
+			switch (byte) {
+				case 0:
+					CFB.dados = CacheInstrucoes.Linha[line].p1;
+					break;
+				case 1:
+					CFB.dados = CacheInstrucoes.Linha[line].p2;
+					break;
+				case 2:
+					CFB.dados = CacheInstrucoes.Linha[line].p3;
+					break;
+				case 3:
+					CFB.dados = CacheInstrucoes.Linha[line].p4;
+					break;
+			}
+		}
+		else {
+			buffer = CFB.address;
+			CMB.address = (buffer >> NUM_BITS_BYTE) << NUM_BITS_BYTE;
+			CMB.controle = OCUPADO;
+			
+			readRAM();
+			writeInstructionCache();
+			
+			buffer = CMB.dados;
+			CFB.dados = buffer;
+			//INCREMENTA CICLOS GASTOS
+		}
+	}
+	else {
+		buffer = CFB.address;
+		CMB.address = (buffer >> NUM_BITS_BYTE) << NUM_BITS_BYTE;
+		CMB.controle = OCUPADO;
+		
+		readRAM();
+		writeInstructionCache();
+		
+		buffer = CMB.dados;
+		CFB.dados = buffer;
+		
+		//INCREMENTA CICLOS GASTOS
+	}
+}
+
+void writeInstructionCache() {			// É sempre pelo CMB
+	unsigned int line, tag, byte;
+	unsigned int buffer, buffer02;		// Simbolizam buffers genéricos
+	
+	line = (CMB.address << NUM_BITS_TAG) >> (32 - NUM_BITS_LINHA);
+	tag = CMB.address >> (32 - NUM_BITS_TAG);
+	byte = (CMB.address << (32 - NUM_BITS_BYTE)) >> (32 - NUM_BITS_BYTE);
+	
+	if (CacheInstrucoes.estado[line] == VALIDO) {
+		buffer = CMB.dados;
+		buffer02 = CMB.address;
+		
+		CMB.address = ((CacheInstrucoes.tag[line] << (32 - NUM_BITS_TAG))|(line << NUM_BITS_BYTE)|(0));
+		//Faz o Write-Back
+		switch (byte) {
+			case 0:
+				CMB.dados = CacheInstrucoes.Linha[line].p1;
+				writeRAM();
+				break;
+			case 1:
+				CMB.dados = CacheInstrucoes.Linha[line].p2;
+				writeRAM();
+				break;
+			case 2:
+				CMB.dados = CacheInstrucoes.Linha[line].p3;
+				writeRAM();
+				break;
+			case 3:
+				CMB.dados = CacheInstrucoes.Linha[line].p4;
+				writeRAM();
+				break;
+		}
+		CMB.dados = buffer;
+		CMB.address = buffer02;
+	}
+	
+	// Escreve dados na cache
+	CacheInstrucoes.tag[line] = tag;
+	CacheInstrucoes.estado[line] = VALIDO;
+	
+	switch (byte) {
+			case 0:
+				CacheInstrucoes.Linha[line].p1 = CMB.dados;
+				break;
+			case 1:
+				CacheInstrucoes.Linha[line].p2 = CMB.dados;
+				break;
+			case 2:
+				CacheInstrucoes.Linha[line].p3 = CMB.dados;
+				break;
+			case 3:
+				CacheInstrucoes.Linha[line].p4 = CMB.dados;
 				break;
 	}
 	
